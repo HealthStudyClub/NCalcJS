1e70e33ae0e1933520d55f4a6bf47ffffa0fbcdb
robmarti12 <robin.martin@cmcitymedia.de>
2024-06-17 10:24:11 +0200

    fix(evaluation): allow for comparisons between non numerical values

--- src/NCalc/Domain/EvaluationVisitor.ts
+++ src/NCalc/Domain/EvaluationVisitor.ts
@@ -126,13 +126,10 @@ export class EvaluationVisitor extends LogicalExpressionVisitor {
             return 1;
         }
 
-        if (typeof a == 'number' || typeof b == 'number') {
-            if (a < b) {
-                return -1;
-            } else if (a > b) {
-                return 1;
-            }
-            return 0;
+        if (a < b) {
+            return -1;
+        } else if (a > b) {
+            return 1;
         } else {
             return a == b ? 0 : 1;
         }
